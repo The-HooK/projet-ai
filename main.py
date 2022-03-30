3b2b97338e25a1d60242cf630b7e05434d436029
The-HooK <joel.2.rovera@gmail.com>
2022-03-30 18:31:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from sklearn.neighbors import KNeighborsClassifier
 
 from mahotas import euler
-from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.io import imread, imread_collection, concatenate_images
 from skimage.color import rgb2gray
 
 
@@ -24,19 +25,26 @@ def getdata():
     data_INC5 = []
     data_INC9 = []
     for i in range(1, 9):
-        data_LED5.append(rgb2gray(imread('../data/LED/5' + str(i) + '.jpg')))
-        data_LED9.append(rgb2gray(imread('../data/LED/9' + str(i) + '.jpg')))
-        data_INC5.append(rgb2gray(imread('../data/INC/5' + str(i) + '.jpg')))
-        data_INC9.append(rgb2gray(imread('../data/INC/9' + str(i) + '.jpg')))
+        data_LED5.append(rgb2gray(imread('../data/LED_cropped_bin/5' + str(i) + '.jpg')))
+        data_LED9.append(rgb2gray(imread('../data/LED_cropped_bin/9' + str(i) + '.jpg')))
+        data_INC5.append(rgb2gray(imread('../data/INC_cropped_bin/5' + str(i) + '.jpg')))
+        data_INC9.append(rgb2gray(imread('../data/INC_cropped_bin/9' + str(i) + '.jpg')))
     # mask = imread()
     global data_ordre0_LED5
     global data_ordre0_LED9
     global data_ordre0_INC5
     global data_ordre0_INC9
-    data_ordre0_LED5 = crop(data_LED5)
-    data_ordre0_LED9 = crop(data_LED9)
-    data_ordre0_INC5 = crop(data_INC5)
-    data_ordre0_INC9 = crop(data_INC9)
+    # data_ordre0_LED5 = crop(data_LED5)
+    # data_ordre0_LED9 = crop(data_LED9)
+    # data_ordre0_INC5 = crop(data_INC5)
+    # data_ordre0_INC9 = crop(data_INC9)
+
+    data_ordre0_LED5 = data_LED5
+    data_ordre0_LED9 = data_LED9
+    data_ordre0_INC5 = [data[:66, :66] for data in data_INC5]
+    data_ordre0_INC9 = [data[:66, :66] for data in data_INC9]
+
+    return data_ordre0_LED5 + data_ordre0_LED9 + data_ordre0_INC5 + data_ordre0_INC9
 
 
 def crop(image):
@@ -48,12 +56,10 @@ def crop(image):
 
 def calcul_moyenne(image):
     if isinstance(image, list):
-        out = np.zeros(len(image))
-        for i in range(len(image)):
-            out[i] = np.mean(image[i])
-        return out
+        return [np.mean(img) for img in image]
     else:
         return np.mean(image)
+
 
 def calcul_var(image):
     if isinstance(image, list):
@@ -61,18 +67,19 @@ def calcul_var(image):
     else:
         return np.var(calcul_projection(image))
 
+
 def calcul_cov(image):
     if isinstance(image, list):
-        return [np.cov(calcul_projection(img), calcul_projection(img.transpose())) for img in image]
+        return [np.cov(calcul_projection(img), calcul_projection(img.T))[1, 0] for img in image]
     else:
-        return np.cov(calcul_projection(image), calcul_projection(image.transpose()))
+        return np.cov(calcul_projection(image), calcul_projection(image.T))[1, 0]
 
 
 def calcul_conv(image):
     if isinstance(image, list):
-        return [sum(calcul_projection(img) * calcul_projection(img.transpose())) for img in image]
+        return [sum(calcul_projection(img) * calcul_projection(img.T)) for img in image]
     else:
-        return sum(calcul_projection(image) * calcul_projection(image.transpose()))
+        return sum(calcul_projection(image) * calcul_projection(image.T))
 
 
 def calcul_projection(image):
@@ -82,87 +89,148 @@ def calcul_projection(image):
         return sum(image)
 
 
-def plot_feature(func):
-    if isinstance(func, list):
-        if len(func) > 3:
-            print("i can only plot features in 3D space max, sorry")
-            return
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.scatter(func[0](data_ordre0_LED5), func[1](data_ordre0_LED5), func[2](data_ordre0_LED5), c='blue')
-        ax.scatter(func[0](data_ordre0_LED9), func[1](data_ordre0_LED9), func[2](data_ordre0_LED9), c='red')
-        ax.scatter(func[0](data_ordre0_INC5), func[1](data_ordre0_INC5), func[2](data_ordre0_INC5), c='green')
-        ax.scatter(func[0](data_ordre0_INC9), func[1](data_ordre0_INC9), func[2](data_ordre0_INC9), c='orange')
-        plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
-        ax.set_xlabel(func[0].__name__)
-        ax.set_ylabel(func[1].__name__)
-        ax.set_zlabel(func[2].__name__)
-        plt.show()
+def plot_images():
+    # album_LED5 = np.concatenate(np.array([data_ordre0_LED5]).reshape((4,2)))
+    names = ['LED5', 'LED9', 'INC5', 'INC9']
+    album = [
+        np.concatenate([np.concatenate(data_ordre0_LED5[:4], axis=1), np.concatenate(data_ordre0_LED5[4:], axis=1)]),
+        np.concatenate([np.concatenate(data_ordre0_LED9[:4], axis=1), np.concatenate(data_ordre0_LED9[4:], axis=1)]),
+        np.concatenate([np.concatenate(data_ordre0_INC5[:4], axis=1), np.concatenate(data_ordre0_INC5[4:], axis=1)]),
+        np.concatenate([np.concatenate(data_ordre0_INC9[:4], axis=1), np.concatenate(data_ordre0_INC9[4:], axis=1)])]
+
+    fig = plt.figure()
+    for i in range(4):
+        plt.subplot(221 + i)
+        plt.imshow(album[i])
+
+        frame1 = fig.gca()
+        frame1.axes.get_xaxis().set_visible(False)
+        frame1.axes.get_yaxis().set_visible(False)
+        plt.gray()
+        plt.title(names[i])
+    plt.show()
+
+
+def plot_proj():
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(range(len(data_ordre0_LED5[1][1])), calcul_projection(np.mean(data_ordre0_LED5, 0)), c='blue')
+    plt.plot(range(len(data_ordre0_LED9[1][1])), calcul_projection(np.mean(data_ordre0_LED9, 0)), c='red')
+    plt.plot(range(len(data_ordre0_INC5[1][1])), calcul_projection(np.mean(data_ordre0_INC5, 0)), c='green')
+    plt.plot(range(len(data_ordre0_INC9[1][1])), calcul_projection(np.mean(data_ordre0_INC9, 0)), c='orange')
+    for i in range(8):
+        plt.plot(range(len(data_ordre0_LED5[1][1])), calcul_projection(data_ordre0_LED5[i]), '--', c='blue', alpha=0.3)
+        plt.plot(range(len(data_ordre0_LED9[1][1])), calcul_projection(data_ordre0_LED9[i]), '--', c='red', alpha=0.3)
+        plt.plot(range(len(data_ordre0_INC5[1][1])), calcul_projection(data_ordre0_INC5[i]), '--', c='green', alpha=0.3)
+        plt.plot(range(len(data_ordre0_INC9[1][1])), calcul_projection(data_ordre0_INC9[i]), '--', c='orange',
+                 alpha=0.3)
+    plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
+    plt.title("Projection sur l'axe horizontal")
+
+    plt.subplot(212)
+    plt.plot(range(len(data_ordre0_LED5[1])), calcul_projection(np.mean([el.T for el in data_ordre0_LED5], 0)),
+             c='blue')
+    plt.plot(range(len(data_ordre0_LED9[1])), calcul_projection(np.mean([el.T for el in data_ordre0_LED9], 0)), c='red')
+    plt.plot(range(len(data_ordre0_INC5[1])), calcul_projection(np.mean([el.T for el in data_ordre0_INC5], 0)),
+             c='green')
+    plt.plot(range(len(data_ordre0_INC9[1])), calcul_projection(np.mean([el.T for el in data_ordre0_INC9], 0)),
+             c='orange')
+    for i in range(8):
+        plt.plot(range(len(data_ordre0_LED5[1])), calcul_projection(data_ordre0_LED5[i].T), '--', c='blue', alpha=0.3)
+        plt.plot(range(len(data_ordre0_LED9[1])), calcul_projection(data_ordre0_LED9[i].T), '--', c='red', alpha=0.3)
+        plt.plot(range(len(data_ordre0_INC5[1])), calcul_projection(data_ordre0_INC5[i].T), '--', c='green', alpha=0.3)
+        plt.plot(range(len(data_ordre0_INC9[1])), calcul_projection(data_ordre0_INC9[i].T), '--', c='orange', alpha=0.3)
+    plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
+    plt.title("Projection sur l'axe vertical")
+
+
+def plot_feature(features, data_x=None):
+    if isinstance(features, list):
+        for func in features:
+            fig = plt.figure()
+            plt.scatter(range(len(data_ordre0_LED5)), func(data_ordre0_LED5), c='blue')
+            plt.scatter(range(len(data_ordre0_LED9)), func(data_ordre0_LED9), c='red')
+            plt.scatter(range(len(data_ordre0_INC5)), func(data_ordre0_INC5), c='green')
+            plt.scatter(range(len(data_ordre0_INC9)), func(data_ordre0_INC9), c='orange')
+            if data_x is None:
+                plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
+            else:
+                plt.scatter(8, func(data_x), marker='x', color='k')
+                plt.legend(['LED5', 'LED9', 'INC5', 'INC9', 'data_x'])
+            plt.title(func.__name__)
+            plt.show()
     else:
-        plt.figure()
+        func = features
+        fig = plt.figure()
         plt.scatter(range(len(data_ordre0_LED5)), func(data_ordre0_LED5), c='blue')
         plt.scatter(range(len(data_ordre0_LED9)), func(data_ordre0_LED9), c='red')
         plt.scatter(range(len(data_ordre0_INC5)), func(data_ordre0_INC5), c='green')
         plt.scatter(range(len(data_ordre0_INC9)), func(data_ordre0_INC9), c='orange')
-        plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
-        plt.xlabel('data')
-        plt.ylabel(func.__name__)
+        if data_x is None:
+            plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
+        else:
+            plt.scatter(8, func(data_x), marker='x', color='k')
+            plt.legend(['LED5', 'LED9', 'INC5', 'INC9', 'data_x'])
+            plt.title(func.__name__)
         plt.show()
 
-getdata()
-"""
-plt.figure()
-plt.subplot(221)
-imshow(data_ordre0_LED5[1])
-plt.subplot(222)
-imshow(data_ordre0_LED9[1])
-plt.subplot(223)
-imshow(data_ordre0_INC5[1])
-plt.subplot(224)
-imshow(data_ordre0_INC9[1])
-plt.show()
-"""
-plot_feature(calcul_moyenne)
-plot_feature(calcul_var)
-plot_feature(calcul_conv)
-
-plot_feature([calcul_moyenne, calcul_var, calcul_conv])
-
-## plot_feature(calcul_cov)
-#
-#noms = ["\delta_x
-for i in [0, 1, 3]:
-    plt.figure()
-    plt.scatter(range(len(data_ordre0_LED5)), [calcul_cov(img).reshape(4)[i] for img in data_ordre0_LED5], c='blue')
-    plt.scatter(range(len(data_ordre0_LED9)), [calcul_cov(img).reshape(4)[i] for img in data_ordre0_LED9], c='red')
-    plt.scatter(range(len(data_ordre0_INC5)), [calcul_cov(img).reshape(4)[i] for img in data_ordre0_INC5], c='green')
-    plt.scatter(range(len(data_ordre0_INC9)), [calcul_cov(img).reshape(4)[i] for img in data_ordre0_INC9], c='orange')
-    plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
-    plt.axis()
 
 
-plt.figure()
-plt.subplot(211)
-plt.plot(range(120), calcul_projection(np.mean(data_ordre0_LED5, 0)), c='blue')
-plt.plot(range(120), calcul_projection(np.mean(data_ordre0_LED9, 0)), c='red')
-plt.plot(range(120), calcul_projection(np.mean(data_ordre0_INC5, 0)), c='green')
-plt.plot(range(120), calcul_projection(np.mean(data_ordre0_INC9, 0)), c='orange')
-for i in range(8):
-    plt.plot(range(120), calcul_projection(data_ordre0_LED5[i]), '--', c='blue', alpha=0.3)
-    plt.plot(range(120), calcul_projection(data_ordre0_LED9[i]), '--', c='red', alpha=0.3)
-    plt.plot(range(120), calcul_projection(data_ordre0_INC5[i]), '--', c='green', alpha=0.3)
-    plt.plot(range(120), calcul_projection(data_ordre0_INC9[i]), '--', c='orange', alpha=0.3)
-plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
+def plot_feature_space(X, y, features, dims=[0, 1, 2]):
+    names = np.unique(y)
+    colors = ['b', 'r', 'g', 'orange', 'k']
+    markers = ['o', 'o', 'o', 'o', 'x']
 
-plt.subplot(212)
-plt.plot(range(120), calcul_projection(np.mean([el.transpose() for el in data_ordre0_LED5], 0)), c='blue')
-plt.plot(range(120), calcul_projection(np.mean([el.transpose() for el in data_ordre0_LED9], 0)), c='red')
-plt.plot(range(120), calcul_projection(np.mean([el.transpose() for el in data_ordre0_INC5], 0)), c='green')
-plt.plot(range(120), calcul_projection(np.mean([el.transpose() for el in data_ordre0_INC9], 0)), c='orange')
-for i in range(8):
-    plt.plot(range(120), calcul_projection(data_ordre0_LED5[i].transpose()), '--', c='blue', alpha=0.3)
-    plt.plot(range(120), calcul_projection(data_ordre0_LED9[i].transpose()), '--', c='red', alpha=0.3)
-    plt.plot(range(120), calcul_projection(data_ordre0_INC5[i].transpose()), '--', c='green', alpha=0.3)
-    plt.plot(range(120), calcul_projection(data_ordre0_INC9[i].transpose()), '--', c='orange', alpha=0.3)
-    print(i)
-plt.legend(['LED5', 'LED9', 'INC5', 'INC9'])
+    if isinstance(X, np.ndarray) and X.ndim == 2 and X.shape[-1] == x.shape[-1]:
+        fig = plt.figure()
+
+        if X.shape[1] == 2:
+            for i, name in enumerate(names):
+                plt.scatter(X[y == name, 0], X[y == name, 1], marker=markers[i],color=colors[i])
+        else:
+            ax = fig.add_subplot(projection='3d')
+            ax.set_zlabel(features[dims[2]].__name__)
+            for i, name in enumerate(names):
+                ax.scatter(X[y == name, dims[0]], X[y == name, dims[1]], X[y == name, dims[2]], marker=markers[i], color=colors[i])
+
+        plt.legend(names)
+        plt.xlabel(features[dims[0]].__name__)
+        plt.ylabel(features[dims[1]].__name__)
+        plt.show()
+    else:
+        print('burp')
+        return 'burp'
+
+
+def knn(X, x, y, k=2):
+    dists = np.sqrt(((X - x) ** 2).sum(axis=1))
+    ind = np.argsort(dists)
+
+    unique, counts = np.unique(y[ind[0]], return_counts=True)
+    result = unique[np.argmax(counts)]
+
+    plot_feature_space(np.vstack((X, x)), np.hstack((y, "data_X = " + result)), features)
+
+    print(result)
+    return result
+
+
+data = getdata()
+data_x = data[0]
+features = [calcul_moyenne, calcul_var, calcul_conv]
+
+plot_images()
+plot_proj()
+#plot_feature(features, data_x)
+
+X = np.array([func(data) for func in features]).T
+x = np.array([func(data_x) for func in features]).T
+y = np.array(["LED5"] * 8 + ["LED9"] * 8 + ["INC5"] * 8 + ["INC9"] * 8)
+
+X_norm = 1 / X.max(axis=0)
+X = X * X_norm
+x = x * X_norm
+
+#plot_feature_space(np.vstack((X, x)), np.hstack((y, ["data_x"])), features)
+
+result = knn(X, x, y, k=5)
